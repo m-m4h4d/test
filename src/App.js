aacc94093bd1b68965c9002a557e3b95c04a0776
Muhammad Mahad <mahadbinashfaq@gmail.com>
2024-07-05 18:39:04 +0500

    Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Box, Typography, Divider } from '@mui/material';
+import CustomerDetails from './components/CustomerDetails';
+import CustomerResidualRisk from './components/CustomerResidualRisk';
+import RiskDistribution from './components/RiskDistribution';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ pb: 10, mb: 10 }}>
+      <Box sx={{ my: 4, p: 2 }}>
+          <Typography variant="h4" align="left"><b>ARA JEWELLERY LLC</b></Typography>
+          <Typography variant='body2' align="left">Office # 402, Dubai Diamond Plaza, Gold Souk Deira</Typography>
+          <Typography variant='body2' align="left">+971 4 3305259</Typography>
+          <Typography variant='body2' align="left">arajewellltd@gmail.com</Typography>
+          <Divider sx={{ my: 1, border: '2px solid' }} />
+          <CustomerDetails />
+          <CustomerResidualRisk />
+          <RiskDistribution />
+          <Divider>&nbsp; ***** END OF CUSTOMER ***** &nbsp;</Divider>
+      </Box>
+    </Container>
   );
 }
 
